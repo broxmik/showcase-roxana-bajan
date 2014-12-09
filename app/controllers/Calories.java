9660145e90c1daa852a9d273254e3a478cb46ff9
Roxana <roxanabajan@gmail.com>
2014-12-09 11:09:28 +0100

    added calories content

--- app/controllers/Calories.java
+++ app/controllers/Calories.java
@@ -1,18 +1,18 @@
 package controllers;
 
 import play.Logger;
+import play.api.Play;
 import play.mvc.Controller;
 import play.mvc.Result;
 import ro.brox.model.Food;
+import scala.Option;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,29 +26,23 @@ public class Calories extends Controller {
 
     public Result getCalories() {
         List<Food> foodList;
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource("public/data/calories.csv");
-        if(resource == null){
-            String message = "Could not find calories.csv";
-            Logger.error(message);
-            return internalServerError(message);
+        Option<File> existingFile = Play.getExistingFile("conf/data/calories.csv", Play.current());
+        if(existingFile.nonEmpty()) {
+            Path path = existingFile.get().toPath();
+            try (Stream<String> lines = Files.lines(path, StandardCharsets.ISO_8859_1)) {
+                foodList = lines.skip(2)
+                        .parallel()
+                        .filter(s -> s != null && !s.trim().isEmpty())
+                        .map(Food::new)
+                        .sorted(Comparator.comparingInt(Food::getkCal).reversed())
+                        .limit(12)
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                Logger.error(e.getMessage(), e);
+                return internalServerError(e.getMessage());
+            }
+            return ok(foodList.toString());
         }
-        try (Stream<String> lines = Files.lines(Paths.get(resource.toURI()), StandardCharsets.ISO_8859_1)) {
-            foodList = lines.skip(2)
-                    .parallel()
-                    .filter(s -> s != null && !s.trim().isEmpty())
-                    .map(Food::new)
-                    .sorted(Comparator.comparingInt(Food::getkCal).reversed())
-                    .limit(12)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            Logger.error(e.getMessage(), e);
-            return internalServerError(e.getMessage());
-        } catch (URISyntaxException e) {
-            Logger.error(e.getMessage(), e);
-            return internalServerError(e.getMessage());
-        }
-
-        return ok(foodList.toString());
+        return internalServerError("Could not find file calories.csv");
     }
 }
